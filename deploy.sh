bc48574170673cb179102ef4e58a28ff3390fae8
Nikita Efanov <utttt9111@gmail.com>
2016-04-13 15:20:57 +0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-# #!/bin/bash
+#!/bin/bash
+server=personal@efanov.me
+client_path=/home/personal/automation-sync-game-experiment
+
+ssh $server "ssh-agent bash --login -c 'source ~/.nvm/nvm.sh && nvm use stable && cd $client_path && git pull && npm install && npm run build'"
